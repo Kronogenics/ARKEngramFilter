b9d8ec2e0396b95b5e80d4a9cc1a2b9f69073b7f
Kronogenics <justin.t.midkiff@gmail.com>
2020-10-11 22:51:04 -0400

    Made the thing

---
+++ static/js/css.js
@@ -0,0 +1,8 @@
+// navbar.js
+
+var css_html = `
+<link rel="stylesheet" href="/static/css/bootstrap.min.css">
+<link rel="stylesheet" href="/static/css/custom.css">
+`
+
+document.documentElement.innerHTML += css_html.trim();
